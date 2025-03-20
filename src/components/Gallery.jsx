1d6101e1ed4737bce2fb322b0c6eca2318fde738
raushan <kraushan022@gmail.com>
2025-03-20 16:45:09 +0530

    lightbox feature to view full size image

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import IMG_4896 from "../assets/IMG_4896.jpg";
 import IMG_4909 from "../assets/IMG_4909.jpg";
 import IMG_5375 from "../assets/IMG_5375.jpg";
@@ -22,6 +22,7 @@ const images = [
 ];
 
 const Gallery = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
   return (
     <section id="gallery" className="py-16 px-6 bg-blue-100 text-gray-700">
       <div className='container mx-auto text-center"'>
@@ -32,16 +33,42 @@ const Gallery = () => {
         {/* Grid Layout */}
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
           {images.map((img, index) => (
-            <div key={index} className="overflow-hidden rounded-xl shadow-lg">
+            <div
+              key={index}
+              className="overflow-hidden rounded-xl shadow-lg cursor-pointer"
+            >
               <img
                 src={img}
                 alt={`baby_moment ${index + 1}`}
                 className="w-full h-60 object-cover rounded-xl transition-transform duration-300 hover:scale-110"
+                onClick={() => setSelectedImage(img)}
               />
             </div>
           ))}
         </div>
       </div>
+
+      {/* Lightbox Modal */}
+      {selectedImage && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
+          onClick={() => setSelectedImage(null)}
+        >
+          <div className="relative p-4 bg-white rounded-lg shadow-lg">
+            <button
+              className="absolute top-2 right-2 text-gray-700 text-2xl cursor-pointer"
+              onClick={() => setSelectedImage(null)}
+            >
+              ✖
+            </button>
+            <img
+              src={selectedImage}
+              alt="Full_Size"
+              className="w-full max-w-lg rounded-b-lg"
+            />
+          </div>
+        </div>
+      )}
     </section>
   );
 };
